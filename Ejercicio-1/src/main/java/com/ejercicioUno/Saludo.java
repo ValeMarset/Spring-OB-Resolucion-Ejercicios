b0473ed11947f8a3ac09771dd4dfb52efb7d4708
Vale Marset <valemarset05@gmail.com>
2022-11-21 22:07:21 -0300

    Ejercicio Uno Terminado

---
+++ Ejercicio-1/src/main/java/com/ejercicioUno/Saludo.java
@@ -0,0 +1,17 @@
+package com.ejercicioUno;
+
+public class Saludo {
+
+
+    public Saludo(){
+
+    }
+
+
+    public String  imprimirSaludo (){
+        return "Hola! Desde imprimirSaludo()";
+    }
+
+
+
+}
