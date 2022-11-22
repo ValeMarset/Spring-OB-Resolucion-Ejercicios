8b9f7200033ad9a58fa8b035ab64d17b8fd8f2d1
Vale Marset <valemarset05@gmail.com>
2022-11-21 22:42:02 -0300

    Ejercicio Tres Terminado

---
+++ Ejercicio-3/ejercicioTres/src/main/java/com/ejercicioTres/EjercicioTresApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicioTres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioTresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioTresApplication.class, args);
+	}
+
+}
---
+++ Ejercicio-3/ejercicioTres/src/test/java/com/ejercicioTres/EjercicioTresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicioTres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioTresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
