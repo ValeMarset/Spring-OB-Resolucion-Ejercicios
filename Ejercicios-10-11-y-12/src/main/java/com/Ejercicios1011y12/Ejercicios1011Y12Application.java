5c1cdb413aa223cffce5b05eb82d7060056a7255
Vale Marset <valemarset05@gmail.com>
2022-11-30 23:07:00 -0300

    Ejercicio 11 en proceso

---
+++ Ejercicios-10-11-y-12/src/main/java/com/Ejercicios1011y12/Ejercicios1011Y12Application.java
@@ -0,0 +1,13 @@
+package com.Ejercicios1011y12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicios1011Y12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicios1011Y12Application.class, args);
+	}
+
+}
---
+++ Ejercicios-10-11-y-12/src/test/java/com/Ejercicios1011y12/Ejercicios1011Y12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ejercicios1011y12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicios1011Y12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
