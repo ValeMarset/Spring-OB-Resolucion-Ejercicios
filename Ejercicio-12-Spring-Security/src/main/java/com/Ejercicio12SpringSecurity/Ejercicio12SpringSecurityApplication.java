dbbf3ae731e3fb2904a875b355b3c97d3bc58d20
Vale Marset <valemarset05@gmail.com>
2022-12-01 15:58:08 -0300

    Ejercicio 12 terminado, está echo como lo hizo Alan en clase, ya que la clase WebSecurityConfigurerAdapter hoy en día está obsoleta

---
+++ Ejercicio-12-Spring-Security/src/main/java/com/Ejercicio12SpringSecurity/Ejercicio12SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.Ejercicio12SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio12SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio12SpringSecurityApplication.class, args);
+	}
+
+}
---
+++ Ejercicio-12-Spring-Security/src/test/java/com/Ejercicio12SpringSecurity/Ejercicio12SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ejercicio12SpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio12SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
